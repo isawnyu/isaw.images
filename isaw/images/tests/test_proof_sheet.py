71476f24ddbcfe1881fa50cb4190aad444d5f48f
Tom Elliott <tom.elliott@nyu.edu>
2015-03-18 16:14:25 -0500

    start work on proof sheet functionality

---
+++ isaw/images/tests/test_proof_sheet.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+nosetests for creating contact sheets for isaw.images packages
+tests code in isaw.images/isaw/images/proof_sheet.py
+"""
+
+from isaw.images import proof_sheet
+import logging
+from nose.tools import assert_equals, assert_not_equal, assert_in, assert_is
+    
+logging.basicConfig(level=logging.DEBUG)
