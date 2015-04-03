ebc76fe4270e7b80192daf60324db38b1550be80
Tom Elliott <tom.elliott@nyu.edu>
2015-04-03 08:44:55 -0500

    overview class definition and initialization stub

--- isaw/images/overview.py
+++ isaw/images/overview.py
@@ -9,5 +9,21 @@ import dominate
 from dominate.tags import *
 import logging
 import package
+import sys
+
+class Overview():
+    """
+    HTML overview page for isaw.images
+    """
+
+    @arglogger
+    def __init__(self, path=None):
+        logger = logging.getLogger(sys._getframe().f_code.co_name)
+        if path is not None:
+            self.__generate__(path)
+        else:
+            logger.warning("Overview.init called with path=None")
+    
 
 
+        
